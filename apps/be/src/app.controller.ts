71d64c07b11c1fb02375506b69cef553e6069cbc
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-11-05 22:47:06 +0900

    feat: DBに対するヘルスチェックを行い、コンテナ同士の起動タイミング調整

--- apps/be/src/app.controller.ts
+++ apps/be/src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Employee } from '@prisma/client';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -12,7 +13,9 @@ export class AppController {
   }
 
   @Get('employee')
+  @ApiOperation({ summary: 'Employee情報を取得する' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'success' })
   getEmployee(): Promise<Employee[]> {
-    return this.appService.employee()
+    return this.appService.employee();
   }
 }
