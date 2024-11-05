63e56cc6febc2fd2a3498e70f3a6a47900efe66b
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-11-05 22:03:34 +0900

    feat: データベースからデータを取得する処理実装

--- apps/be/src/app.controller.ts
+++ apps/be/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { employee } from '@prisma/client';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('employee')
+  getEmployee(): Promise<employee[]> {
+    return this.appService.employee()
   }
 }
--- apps/be/src/app.service.ts
+++ apps/be/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { employee } from '@prisma/client';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) { }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async employee(): Promise<employee[]> {
+    return this.prisma.employee.findMany()
   }
 }
