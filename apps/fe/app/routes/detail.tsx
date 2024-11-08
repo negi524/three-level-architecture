2317b17e49089c823cf17990f98636d49f9fb5f8
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-11-08 23:08:32 +0900

    feat: 外部のAPIをフェッチするパターン追加

--- apps/fe/app/routes/detail.tsx
+++ apps/fe/app/routes/detail.tsx
@@ -1,7 +1,28 @@
+import { useLoaderData } from "@remix-run/react";
+
+interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+
+export const loader = async (): Promise<Post[]> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json() as Post[]
+  return posts;
+};
+
 export default function Detail() {
+  const posts = useLoaderData() as Post[]
   return (
-    <div className="flex h-screen">
+    <div>
       <h1>Detail Page</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>title: {post.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
