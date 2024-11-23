b42b21defaf14840feaaa74e246cbb800371f4ce
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-11-23 13:12:19 +0900

    feat: bcryptjsを使ってパスワードをハッシュ化する

--- apps/be/src/user/user.service.ts
+++ apps/be/src/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import * as bcryptjs from 'bcryptjs';
 import User from 'src/user/domain/user';
 
 @Injectable()
@@ -6,6 +7,13 @@ export class UserService {
   constructor() {}
 
   async getUser(): Promise<User> {
+    const saltOrRounds = await bcryptjs.genSalt();
+    const password = 'random_password';
+    const hash = await bcryptjs.hash(password, saltOrRounds);
+    Logger.log({ password });
+    Logger.log({ hash });
+    const isMatch = await bcryptjs.compare(password, hash);
+    Logger.log({ isMatch });
     const user = new User(1, 'fuga');
     return new Promise((resolve, reject) => {
       const success = true;
