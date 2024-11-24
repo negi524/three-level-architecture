8beb5109ce019fd408f6850bdd2f7f99f51d4612
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-11-24 16:00:22 +0900

    feat: 初期データ投入

--- apps/db/initdb.d/2_data.sql
+++ apps/db/initdb.d/2_data.sql
@@ -1,9 +1,51 @@
 USE sample;
 
-INSERT INTO employee (id, name) VALUES (1, "山田");
-INSERT INTO employee (id, name) VALUES (2, "鈴木");
+-- user_name=aaa, password=password
+INSERT INTO
+  account (user_name, password_hash, salt)
+VALUES
+  (
+    'aaa',
+    '$2a$10$UhHbeYW/0kl94zZR//zMtOt4FCZ3OFA07qa5ED/QTG/jXzD7H6SlW',
+    '$2a$10$UhHbeYW/0kl94zZR//zMtO'
+  );
 
-INSERT INTO skill (employee_id, language) VALUES (1, "JavaScript");
-INSERT INTO skill (employee_id, language) VALUES (1, "HTML");
-INSERT INTO skill (employee_id, language) VALUES (1, "CSS");
-INSERT INTO skill (employee_id, language) VALUES (2, "Java");
+-- user_name=bbb, password=password
+INSERT INTO
+  account (user_name, password_hash, salt)
+VALUES
+  (
+    'bbb',
+    '$2a$10$0yaSxRhvloRTvPusX/vbxe8JyZBqfdWlDgZ1UOAy.M4wtdaXiYI.i',
+    '$2a$10$0yaSxRhvloRTvPusX/vbxe'
+  );
+
+INSERT INTO
+  employee (id, name)
+VALUES
+  (1, "山田");
+
+INSERT INTO
+  employee (id, name)
+VALUES
+  (2, "鈴木");
+
+INSERT INTO
+  skill (employee_id, language)
+VALUES
+  (1, "JavaScript");
+
+INSERT INTO
+  skill (employee_id, language)
+VALUES
+  (1, "HTML");
+
+INSERT INTO
+  skill (employee_id, language)
+VALUES
+  (1, "CSS");
+
+INSERT INTO
+  skill (employee_id, language)
+VALUES
+  (2, "Java");
